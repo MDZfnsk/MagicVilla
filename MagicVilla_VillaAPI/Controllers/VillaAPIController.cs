ebe5440ed67ceba4da5ae2c0cb0387a5bb2760eb
Madhusha Fonseka <madhushafonseka@gmail.com>
2023-03-27 07:11:55 +0530

    using ActionResult

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -18,13 +18,43 @@ namespace MagicVilla_VillaAPI.Controllers
         }
 
 
-        //Get one Villa by Id
-        //[HttpGet("id")]
-        [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
-        {
 
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        ///* START :- First Implementation without ActionResult */
+
+        ////Get one Villa by Id
+        ////[HttpGet("id")]
+        //[HttpGet("{id:int}")]
+        //public VillaDTO GetVilla(int id)
+        //{
+
+        //    return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        //}
+
+        ///* END :- First Implementation without ActionResult */
+        ///
+
+
+
+        /* START :- Implementation with ActionResult */
+        
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
         }
+
+        /* END :- Implementation with ActionResult */
     }
 }
