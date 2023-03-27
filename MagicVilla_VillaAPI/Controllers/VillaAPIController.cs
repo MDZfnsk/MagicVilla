fd993ba6adbc7ed2442e9bf43fcae3f80a314b2a
Madhusha Fonseka <madhushafonseka@gmail.com>
2023-03-27 06:56:17 +0530

    Initial GetVillas() & GetVilla(int id) implementation

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,30 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("/api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        //Get all Villas
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas() 
+        {
+
+            return VillaStore.villaList;
+        }
+
+
+        //Get one Villa by Id
+        //[HttpGet("id")]
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
